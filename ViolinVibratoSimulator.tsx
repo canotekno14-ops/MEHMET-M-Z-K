b62f77127c96d49cf7e92224c0f1afbd5ec5d3eb
canotekno14-ops <canotekno14@gmail.com>
2026-08-17 12:03:19 +0300

    Update ViolinVibratoSimulator.tsx

--- ViolinVibratoSimulator.tsx
+++ ViolinVibratoSimulator.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-import { playViolinWithCustomVibrato } from '../audio/synth';
+import { playViolinWithCustomVibrato } from './audio/synth';
 import {
   Play,
   Square,
