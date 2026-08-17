358aee3feb2fbd0e796cc843e24ce48ccdd24c59
canotekno14-ops <canotekno14@gmail.com>
2026-08-17 10:49:39 +0300

    Update PianoMasterclass.tsx

--- PianoMasterclass.tsx
+++ PianoMasterclass.tsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { Language, PianoKeyData } from './types';
 import { TRANSLATIONS } from './translations';
-import { generate88Keys, PIANO_PEDALS, PIANO_ANATOMY, HANON_EXERCISES } from '../data/pianoData';
-import { CHORDS } from '../data/musicTheoryData';
+import { generate88Keys, PIANO_PEDALS, PIANO_ANATOMY, HANON_EXERCISES } from './data/pianoData';
+import { CHORDS } from './data/musicTheoryData';
 import { playPianoNote, noteToFrequency } from '../audio/synth';
 import { Disc, Volume2, Sparkles, Layers, Shield } from 'lucide-react';
 
